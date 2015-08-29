c35471dac3634e0bd2731a792c7ec2e8c4e19202
Diego Garcia <dm_garcia3@hotmail.com>
2015-08-29 09:26:09 -0300

    Assets

---
+++ Such Studios Game SPJam2015/Assets/PlayerAttributes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttributes : MonoBehaviour {
+
+	private int vidas = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D (Collider2D col){
+	
+		if (col.gameObject.name == "tiroInimigo") {
+			vidas--;
+			Destroy(col.gameObject);
+		}
+	
+	}
+}
--- Such Studios Game SPJam2015/Assets/Scripts/PlayerControllerScript.cs
+++ Such Studios Game SPJam2015/Assets/Scripts/PlayerControllerScript.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerControllerScript : MonoBehaviour {
 
 	private const float speed = 5.0f;
+	public GameObject tiroJogador;
+	public bool disparando = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,19 +17,19 @@ public class PlayerControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		/*if (Input.GetButtonDown("Right")){
-			transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
-		}
 
-		if (Input.GetButtonDown("Left")){
-			transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
-		}*/
-
-
-		if (Input.GetButtonDown("Fire1")){
+		if (Input.GetButton ("Fire1")) {
 			//ToDo: Poder Especial
-			Debug.Log("Fogo!");
+			disparando = true;
 		}
+		else {
+			disparando = false;
+		}
+
+		if (disparando) {
+			Instantiate(tiroJogador, transform.position, Quaternion.Euler(0, 0, 90));
+		}
+		
 
 		transform.position += new Vector3 (Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
 
